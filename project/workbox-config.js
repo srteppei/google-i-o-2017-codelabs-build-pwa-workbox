4da4e1549576bc3a906ca7d433c203b789bc1389
Sr.Teppei <jesus.hernanz.mateo@gmail.com>
2018-03-31 12:30:12 +0200

    5. Reinject and update manigest

--- project/workbox-config.js
+++ project/workbox-config.js
@@ -1,8 +1,15 @@
 module.exports = {
   "globDirectory": "build/",
   "globPatterns": [
-    "**/*.css"
+    "**/*.css",
+    "index.html",
+    "js/animation.js",
+    "images/home/*.jpg",
+    "images/icon/*.svg"
   ],
+  "swSrc": "src/sw.js",
   "swDest": "build/sw.js",
-  "swSrc": "src/sw.js"
+  "globIgnores": [
+    "../workbox-config.js"
+  ]
 };
